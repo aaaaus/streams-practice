a088ac5895a5f955aed5e48ca47651357f52ab49
aaaaus <ausluft@gmail.com>
2019-04-24 12:14:21 -0400

    google auth component state added, frontend listens for changes in sign in status

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -6,18 +6,39 @@ const googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID
 
 class GoogleAuth extends React.Component {
 
+  state = { isSignedIn: null }
+
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
         clientId: googleClientId,
         scope: 'email'
+      }).then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.setState({ isSignedIn: this.auth.isSignedIn.get()  });
+        this.auth.isSignedIn.listen(this.onAuthChange);
       })
     });
   }
 
+  //function is invoked by listen function on auth.isSignedIn object, updates status without page reload
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+  }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return null;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in!</div>;
+    } else {
+      return <div>I am not signed in</div>;
+    }
+  }
+
   render() {
     return (
-      <div>GoogleAuth</div>
+      <div>{this.renderAuthButton()}</div>
     )
   }
 }
