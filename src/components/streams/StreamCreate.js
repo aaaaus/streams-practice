07943805bdc16caa0ad5c2d2aa2bfd195a695927
aaaaus <ausluft@gmail.com>
2019-04-29 17:26:07 -0400

    StreamCreate component: customized form fields and set up form submission

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -3,17 +3,30 @@ import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
 
-  renderInput({ input }) {
+  //to better understand object, remove destructuring and console log formProps
+  renderInput({ input, label }) {
+    //console.log(formProps);
+
     // return <input onChange={formProps.input.onChange} value={formProps.input.value}/>
     // return <input {...formProps.input} />
-    return <input {...input} /> //destructured formProps argument
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} /> {/* destructured formProps argument */}
+      </div>
+    )
+  }
+
+  onSubmit(formValues) {
+    console.log(formValues);
   }
 
   render() {
     return (
-      <form>
-        <Field name="title" component={this.renderInput} />
-        <Field name="description" component={this.renderInput} />
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field name="description" component={this.renderInput} label="Enter Description"/>
+        <button className="ui button primary">Submit</button>
       </form>
     )
   }
