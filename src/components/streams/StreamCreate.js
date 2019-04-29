a5cd9d0dfb68641cf9d6761d90a1202f692e7c7c
aaaaus <ausluft@gmail.com>
2019-04-29 17:10:11 -0400

    StreamCreate component: wired up input fields to redux-form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+class StreamCreate extends React.Component {
 
-export default StreamCreate;
+  renderInput({ input }) {
+    // return <input onChange={formProps.input.onChange} value={formProps.input.value}/>
+    // return <input {...formProps.input} />
+    return <input {...input} /> //destructured formProps argument
+  }
+
+  render() {
+    return (
+      <form>
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
+      </form>
+    )
+  }
+}
+
+export default reduxForm({
+  form: 'StreamCreate'
+})(StreamCreate);
